66c28467e3982b7203a5447e50b9e3f546bda9fb
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2020-10-08 12:29:17 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -23,12 +23,12 @@ $MONGO["servers"][$i]["mongo_name"] = "Localhost";//mongo server name
 $MONGO["servers"][$i]["mongo_host"] = "127.0.0.1";//mongo host
 $MONGO["servers"][$i]["mongo_port"] = "27017";//mongo port
 $MONGO["servers"][$i]["mongo_timeout"] = 0;//mongo connection timeout
-// $MONGO["servers"][$i]["mongo_db"] = "iptv_platform_services";//default mongo db to connect, works only if mongo_auth=false
-$MONGO["servers"][$i]["mongo_user"] = "admin";//mongo authentication user name, works only if mongo_auth=false
-$MONGO["servers"][$i]["mongo_pass"] = "1234567890";//mongo authentication password, works only if mongo_auth=false
-$MONGO["servers"][$i]["mongo_auth"] = true;//enable mongo authentication?
+//$MONGO["servers"][$i]["mongo_db"] = "MONGO_DATABASE";//default mongo db to connect, works only if mongo_auth=false
+//$MONGO["servers"][$i]["mongo_user"] = "MONGO_USERNAME";//mongo authentication user name, works only if mongo_auth=false
+//$MONGO["servers"][$i]["mongo_pass"] = "MONGO_PASSWORD";//mongo authentication password, works only if mongo_auth=false
+$MONGO["servers"][$i]["mongo_auth"] = false;//enable mongo authentication?
 
-$MONGO["servers"][$i]["control_auth"] = false;//enable control users, works only if mongo_auth=false
+$MONGO["servers"][$i]["control_auth"] = true;//enable control users, works only if mongo_auth=false
 $MONGO["servers"][$i]["control_users"]["admin"] = "admin";//one of control users ["USERNAME"]=PASSWORD, works only if mongo_auth=false
 
 $MONGO["servers"][$i]["ui_only_dbs"] = "";//databases to display
